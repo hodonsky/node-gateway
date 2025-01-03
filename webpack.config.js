917f87b8effa667cf8a12c474f05748801158d5a
Eric Hodonsky <relic.melex@gmail.com>
2025-01-03 12:31:35 -0800

    Moving to self registration pickup for v3.0

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { resolve } = require( "path" )
 const TerserPlugin = require( "terser-webpack-plugin" )
 
 module.exports = {
-  devtool: "inline-source-map",
+  devtool: "source-map",
   entry: "./lib/Gateway.js",
   externals: [ require( "webpack-node-externals" )() ],
   mode: "production",
@@ -33,17 +33,17 @@ module.exports = {
   target:"node",
   module: {
     rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: "eslint-loader",
-        options: {
-          fix: true,
-          failOnError: true,
-          configFile: resolve( __dirname, ".eslintrc" )
-        }
-      },
+      // {
+      //   enforce: "pre",
+      //   test: /\.js$/,
+      //   exclude: /(node_modules)/,
+      //   loader: "eslint-webpack-plugin",
+      //   options: {
+      //     fix: true,
+      //     failOnError: true,
+      //     configFile: resolve( __dirname, ".eslintrc" )
+      //   }
+      // },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
